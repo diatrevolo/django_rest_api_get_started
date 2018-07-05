1ebea936db4045169046ec361c05c9085f326d47
Roberto <roberto@AVC-MBP15-ROG.local>
2018-07-05 16:50:11 -0400

    Added profile listing and creating feature

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
+
 urlpatterns = [
     url(r'^hello-view/', views.HelloAPIView.as_view()),
     url(r'', include(router.urls)),
